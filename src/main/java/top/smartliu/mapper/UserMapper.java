a72bf5eda8689db694a2a38ce8a893c629ea8328
Tianqing Liu <liutq@hollycrm.com>
2023-02-11 20:38:07 +0800

    使用注解的方式

--- src/main/java/top/smartliu/mapper/UserMapper.java
+++ src/main/java/top/smartliu/mapper/UserMapper.java
@@ -1,9 +1,12 @@
 package top.smartliu.mapper;
 
 import top.smartliu.pojo.User;
-
+import org.apache.ibatis.annotations.Select;
 import java.util.List;
 
 public interface UserMapper {
     List<User> selectAll();
+
+    @Select("select * from tb_user where id  = #{id}")
+    User selectById(int id);
 }
